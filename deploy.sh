311442da8adbfa411e0a86375f9964cdd0fec688
Saravnihon <sarav.nihon@gmail.com>
2025-05-26 17:02:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker login -u saravnihon -p Fujitsu@12345
-docker tag finalproject saravnihon/finalprojectrepo1
-docker push saravnihon/finalprojectrepo1
+docker tag finalproject saravnihon/dev
+docker push saravnihon/dev
